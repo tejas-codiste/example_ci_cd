f0c9a9f312689f9b776fde862ec438faa0c828b8
hasankarli <karlihasann@gmail.com>
2024-09-04 23:21:39 +0300

    chore: Update MyHomePage title style and font size

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+
 import '../flavors.dart';
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(F.title),
+        title: Text(F.title, style: const TextStyle(color: Colors.blue)),
       ),
       body: Center(
         child: Text(
           'Hello ${F.title}',
+          style: const TextStyle(fontSize: 24.0),
         ),
       ),
     );
